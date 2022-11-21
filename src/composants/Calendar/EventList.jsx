861d2b9c0651f8bdd713f4815b2786a185620d64
amine.alami <amine.alami@amj-groupe.com>
2022-11-21 18:20:28 +0100

    retouch

--- src/composants/Calendar/EventList.jsx
+++ src/composants/Calendar/EventList.jsx
@@ -7,14 +7,19 @@ export default function EventList({ events, date, props }) {
         props(events.filter(ev => ev.id !== e.id ));
     }
 
+    if (events.filter(e => moment(e.date).isSame(date, 'year') && moment(e.date).isSame(date, 'month')).length === 0) {
+        return (
+            <span className='no-event'>Aucun evenement pour ce mois.</span>
+        )
+    }
+
     return (
         events.filter(e => moment(e.date).isSame(date, 'year') && moment(e.date).isSame(date, 'month'))
         .map((e, i) => {
             return(
-                // <p key={i}>{e.title} , {e.comment} , {e.date}</p>
                 <div key={e.id} className="shadow event" >
                     <div className="event-date" >
-                        <div>
+                        <div className={ moment(e.date).isSame(moment(date).format("YYYY-MM-DD")) ? 'today-event' : ''} >
                             <span className="day">{ moment(e.date).format("DD") }</span>
                             <span className="month">{ moment(e.date).format("MMM") }</span>
                         </div>
